74634a8e5ce67a7e75d65d04291e564b7a0e84c2
ChangHwan Kim <broadlight.kim@gmail.com>
2023-09-04 19:21:36 +0900

    disable virtual line in nvim_context_vt

--- lua/plugins/nvim_context_vt.lua
+++ lua/plugins/nvim_context_vt.lua
@@ -1,4 +1,10 @@
 return {
     "haringsrob/nvim_context_vt",
     event = "BufReadPost",
+    opts = {
+        disable_virtual_lines = true,
+    },
+    config = function(_, opts)
+        require("nvim_context_vt").setup(opts)
+    end,
 }
