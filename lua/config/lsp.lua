d5b2451b587691518238d4ff26ced72b9d9c4d3a
ChangHwan Kim <broadlight.kim@gmail.com>
2024-07-03 22:34:26 +0900

    enable lsp inlay hints

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -25,6 +25,9 @@ local on_attach = function(client, bufnr)
   if client.server_capabilities.documentSymbolProvider then
     navic.attach(client, bufnr)
   end
+  if client.server_capabilities.inlayHintProvider then
+    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr, })
+  end
 end
 
 -----------------------------------------------------------
